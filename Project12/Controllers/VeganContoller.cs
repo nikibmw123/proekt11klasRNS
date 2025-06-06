e28058a7877df2428207482579198d2128c63d00
nikibmw123 <nikolaih602@gmail.com>
2025-06-06 13:57:18 +0300

    Vtoro kachvane

---
+++ Project12/Controllers/VeganContoller.cs
@@ -0,0 +1,59 @@
+﻿using Project12.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project12.Controllers
+{
+    internal class VeganContoller
+    {
+        private ProjectDbContext DbContext = new ProjectDbContext();
+
+        public Vegan Get(int id)
+        {
+            Vegan findedVegan = DbContext.Vegans.Find(id);
+            if (findedVegan != null)
+            {
+                DbContext.Entry(findedVegan).Reference(x => x.VeganTypes).Load();
+            }
+            return findedVegan;
+        }
+
+        public List<Vegan> GetAll()
+        {
+            return DbContext.Vegans.Include("VeganTypes").ToList();
+        }
+
+
+        public void Create(Vegan vegan)
+        {
+            DbContext.Vegans.Add(vegan);
+            DbContext.SaveChanges();
+        }
+
+        public void Update(int id, Vegan vegan)
+        {
+            Vegan findedVegan = DbContext.Vegans.Find(id);
+            if (findedVegan == null)
+            {
+                return;
+            }
+            findedVegan.Price = vegan.Price;
+            findedVegan.Name = vegan.Name;
+            findedVegan.VeganTypes = vegan.VeganTypes;
+            findedVegan.Description = vegan.Description;
+            findedVegan.VeganTypeId= vegan.VeganTypeId;
+            DbContext.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            Vegan findedVegan = DbContext.Vegans.Find(id);
+            DbContext.Vegans.Remove(findedVegan);
+            DbContext.SaveChanges();
+        }
+    }
+}
+
---
+++ Project12/Controllers/VeganTypeController.cs
@@ -0,0 +1,56 @@
+﻿using Project12.FruitShopDataSetTableAdapters;
+using Project12.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project12.Controllers
+{
+    internal class VeganTypeController
+    {
+
+
+        private ProjectDbContext DbContext = new ProjectDbContext();
+
+        public VeganType Get(int id)
+        {
+                VeganType findedVeganType = DbContext.VeganTypes.Find(id);
+
+            return findedVeganType;
+        }
+
+        public List<VeganType> GetAll()
+        {
+            return DbContext.VeganTypes.ToList();
+        }
+
+
+        public void Create(VeganType vegan)
+        {
+            DbContext.VeganTypes.Add(vegan);
+            DbContext.SaveChanges();
+        }
+
+        public void Update(int id, VeganType vegan)
+        {
+            VeganType findedVeganType = DbContext.VeganTypes.Find(id);
+            if (findedVeganType == null)
+            {
+                return;
+            }
+            findedVeganType.Name = vegan.Name;
+
+            DbContext.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            VeganType findedVeganType = DbContext.VeganTypes.Find(id);
+            DbContext.VeganTypes.Remove(findedVeganType);
+            DbContext.SaveChanges();
+        }
+    }
+}
+
